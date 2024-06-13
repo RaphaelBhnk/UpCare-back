ceecefc4bf1a4fda6e84d026d03349de1a4ba36b
RaphaelBhnk <raphael.bouhnik@edu.ece.fr>
2024-06-13 16:59:17 +0200

    Cleaned app working

---
+++ README.md
@@ -0,0 +1,1 @@
+# UpCare-back
--- routes/route.js
+++ routes/route.js
@@ -5,7 +5,4 @@ const router = express.Router();
 //router.use(checkAuth);
 router.post("/psy", controller.getPsy);
 router.post("/form", controller.sub);
-router.get("/version", (req, res) => {
-  res.json({ version: "1.0.2" }); // Update this with each deployment
-});
 module.exports = router;
