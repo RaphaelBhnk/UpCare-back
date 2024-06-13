e3bb21205a588ebdb270982b8374e24ec983cb7a
RaphaelBhnk <raphael.bouhnik@edu.ece.fr>
2024-06-13 14:04:12 +0200

    Update version psy

--- routes/route.js
+++ routes/route.js
@@ -3,10 +3,7 @@ const controller = require("../controllers/controller");
 const router = express.Router();
 
 //router.use(checkAuth);
-// router.post("/psy", controller.getPsy);
-router.post("/psy", (req, res) => {
-  res.json({ test: "result" });
-});
+router.post("/psy", controller.getPsy);
 router.post("/form", controller.sub);
 router.get("/version", (req, res) => {
   res.json({ version: "1.0.2" }); // Update this with each deployment
